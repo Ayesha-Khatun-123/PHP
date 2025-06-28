3134767debced79a4eebddf180bdd36ab581592e
Ayesha Khatun <ayesha@example.com>
2025-06-28 10:06:52 -0700

    search emp

--- searchEmp.php
+++ searchEmp.php
@@ -17,11 +17,25 @@
        echo "The total number of row:".$n."<br>";
        
       if($n>0){
-           while($row = mysqli_fetch_row($record))
-           {
-            echo $row[0]." ".$row[1]." ".$row[2]." <br>";
-           }
+      //  while($row = mysqli_fetch_assoc($record))
+      //echo"$row[empid]." ".$row[empname]." ".$row[empsalary]
+     // while($row = mysqli_fetch_row($record))
+           //{
+            //echo $row[0]." ".$row[1]." ".$row[2]." <br>";
+           //}
+      echo "<table border='1'>";
+echo "<tr><th>Emp Id</th> <th>Emp Name</th> <th>Emp Salary</th></tr>";
+while($row = mysqli_fetch_row($record)) {
+    echo "<tr>";
+    echo "<td>" . $row[0] . "</td>";
+    echo "<td>" . $row[1] . "</td>";
+    echo "<td>" . $row[2] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
       }
+
+
       else{
          echo "Record not found";
       }
